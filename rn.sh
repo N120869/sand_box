127b7b0b69071f3f0422486a7b8972fe910eb001
Pavitran Kalla <pavikalla@gmail.com>
2017-06-26 21:10:58 +0530

    Update rn.sh

--- rn.sh
+++ rn.sh
@@ -1,3 +1,3 @@
-echo "y" | android update sdk --no-ui --all --filter "tools,build-tools-23.0.1,build-tools-25.0.2,android-23,android-23,extra-android-m2repository,extra-google-m2repository"
+echo "y" | android update sdk --no-ui --all --filter "tools,build-tools-23.0.1,build-tools-25.0.2,android-23,android-25,extra-android-m2repository,extra-google-m2repository"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && sudo apt-get install -y nodejs
 echo "y" | sudo apt-get install libqt5widgets5
